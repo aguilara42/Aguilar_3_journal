7530f459f9cec29d5aab4efaf7d51a53ce06f466
D22 <aguilara42@ninja23.d22.lan>
2016-04-18 11:04:17 -0500

    Monfay

---
+++ sample.asm
@@ -0,0 +1,20 @@
+global _start
+
+        section .text
+_start:
+        ; write(1, message, 4)
+        mov     rax, 1
+        mov     rdi, 1
+        mov     rsi, message
+        mov     rdx, 15
+        syscall
+
+        ; exit(0)
+        mov     eax, 60
+        xor     rdi, rdi
+        syscall
+message:
+        db      "Aldo", 10
+
+
+
